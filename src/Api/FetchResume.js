89a4c3d2d4d8a2e504cf561b27e27d50fe03664f
Nick Bond <bond.n@husky.neu.edu>
2018-04-29 16:53:05 -0400

    writes fetchResume function

--- src/Api/FetchResume.js
+++ src/Api/FetchResume.js
@@ -3,8 +3,6 @@ import Api from './Api';
 
 export default async function() {
   const api = await Api();
-  const response = await api.query(
-    Prismic.Predicates.at('document.type', 'resume'));
-  console.log(response.results[0].data);
+  const response = await api.query(Prismic.Predicates.at('document.type', 'resume'));
   return response.results[0].data;
 }
