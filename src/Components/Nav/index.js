2679536186241c837c2b23339f6eac99d0e1f20e
Nick Bond <bond.n@husky.neu.edu>
2018-04-30 11:54:37 -0400

    adds nav link hover animations and active state

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import './Nav.css';
 
-export default class extends React.Component {
+class Nav extends React.Component {
 
   render() {
+    const {pathname} = this.props.location;
     return (
       <div id="Nav">
         <h1 className="nav-title"><Link className="nav-title-link" to="/">Julia Hechtman</Link></h1>
         <div className="nav-links">
-          <Link className="nav-link" to="/">Work</Link>
-          <Link className="nav-link" to="/resume">Resume</Link>
+          <Link className={`nav-link ${pathname === "/" && "nav-link-active"}`} to="/">Work</Link>
+          <Link className={`nav-link ${pathname === "/resume" && "nav-link-active"}`} to="/resume">Resume</Link>
         </div>
       </div>
     );
-
   }
 }
+
+export default withRouter(props => <Nav {...props}/>);
