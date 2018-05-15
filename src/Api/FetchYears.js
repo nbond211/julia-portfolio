49c0ebe38e55b5b6da823f2e8a3e5965c622f9c1
Nick Bond <bond.n@husky.neu.edu>
2018-05-14 20:25:57 -0400

    filters out corrupt data in api

--- src/Api/FetchYears.js
+++ src/Api/FetchYears.js
@@ -5,10 +5,16 @@ function getPortfolioPiece(content) {
   const portfolioPiece = content['portfolio_piece'];
   const {id, type} = portfolioPiece;
   let {data} = portfolioPiece;
-  data = {...data, title: data.title[0].text}
+  data = {...data, title: data.title[0].text};
   data.id = id;
   data.type = type;
   return data;
+}
+
+function filterCorruptData(content) {
+  const portfolioPiece = content['portfolio_piece'];
+  const {data} = portfolioPiece;
+  return Boolean(data);
 }
 
 export default async function() {
@@ -18,5 +24,9 @@ export default async function() {
   );
   const {results} = response;
 
-  return results.map(val => {return {...val.data, content: val.data.content.map(content => getPortfolioPiece(content))}});
+  return results
+    .filter(val => val.data.content[0])
+    .map(val => {return {...val.data, content: val.data.content
+        .filter(filterCorruptData)
+        .map(content => getPortfolioPiece(content))}});
 }
