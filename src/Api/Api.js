2cf67217b6bcfd92d198b37790bb48c9038e584b
Nick Bond <bond.n@husky.neu.edu>
2018-05-31 14:10:14 +0000

    reconfigures app to use environment variables

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,6 +2,8 @@ import Prismic from 'prismic-javascript';
 import PrivateKeys from "../PrivateKeys";
 
 export default async function() {
-  const {apiEndpoint, accessToken} = PrivateKeys;
+  let {apiEndpoint, accessToken} = PrivateKeys;
+  if (!apiEndpoint) apiEndpoint = process.env.API_ENDPOINT;
+  if (!accessToken) accessToken = process.env.ACCESS_TOKEN;
   return await Prismic.api(apiEndpoint, {accessToken});
 }
