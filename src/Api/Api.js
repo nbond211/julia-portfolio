d148c18df9c89fa776edcdc871d6aaf4b0e6e155
Nick Bond <bond.n@husky.neu.edu>
2018-05-31 14:23:56 +0000

    tests change in API.js

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -3,9 +3,9 @@ import Prismic from 'prismic-javascript';
 export default async function() {
   let PrivateKeys;
   if (!process.env.API_ENDPOINT) {
-    PrivateKeys = require("../PrivateKeys");
+    PrivateKeys = false; //require("../PrivateKeys");
   }
-  let {apiEndpoint, accessToken} = PrivateKeys;
+  // let {apiEndpoint, accessToken} = PrivateKeys;
   if (!apiEndpoint) apiEndpoint = process.env.API_ENDPOINT;
   if (!accessToken) accessToken = process.env.ACCESS_TOKEN;
   return await Prismic.api(apiEndpoint, {accessToken});
