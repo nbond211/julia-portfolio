e2df8345b1d97a19094a0fdaa4461bb1c71d39d0
Nick Bond <bond.n@husky.neu.edu>
2018-06-01 15:16:01 +0000

    makes wide images take up more width in grid

--- src/Components/Photo/index.js
+++ src/Components/Photo/index.js
@@ -17,10 +17,10 @@ export default class extends React.Component {
     const {height, width} = preview_image.dimensions;
 
     const isTall = height / width >= 1.2;
-
+    const isWide = width / height >= 2.4;
 
     return (
-        <div onClick={this.onClick} className={`photo-column col-sm-${isTall ? 5 : 6} col-md-${isTall ? 3 : 4}`}>
+        <div onClick={this.onClick} className={`photo-column col-sm-${isTall ? 5 : 6} col-md-${isTall ? 3 : isWide ? 6 : 4}`}>
           <div className="photo-container">
             <LazyLoad once>
               <div className="photo-preview-image">
